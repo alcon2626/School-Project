9f6b0e7ff2f53e845cf7356420f2a38e5a7de19b
Leonel Gonzalez <alcon2626@gmail.com>
2019-01-23 18:39:17 -0500

    add it geolocation to the app inventory

--- School Calculator/Inventory.xaml.cs
+++ School Calculator/Inventory.xaml.cs
@@ -9,6 +9,9 @@ using System.Threading.Tasks;
 using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using System.Threading.Tasks;
+using Windows.Devices.Geolocation;
+
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -23,6 +26,39 @@ namespace School_Calculator
         {
             this.InitializeComponent();
         }
+        //GEOLOCATION Stuff
+        private async void OneShotLocation_Click(object sender, RoutedEventArgs e)
+        {
+
+            Geolocator geolocator = new Geolocator();
+            geolocator.DesiredAccuracyInMeters = 50;
+
+            try
+            {
+                Geoposition geoposition = await geolocator.GetGeopositionAsync(
+                    maximumAge: TimeSpan.FromMinutes(5),
+                    timeout: TimeSpan.FromSeconds(10)
+                    );
+
+                LatitudeTextBlock.Text = geoposition.Coordinate.Latitude.ToString("0.00");
+                LongitudeTextBlock.Text = geoposition.Coordinate.Longitude.ToString("0.00");
+                StatusTextBlock.Text = "Found GPS Location.";
+            }
+            catch (Exception ex)
+            {
+                if ((uint)ex.HResult == 0x80004004)
+                {
+                    // the application does not have the right capability or the location master switch is off
+                    StatusTextBlock.Text = "GPS location is disabled in phone settings.";
+                }
+                //else
+                {
+                    // something else happened acquring the location
+                    StatusTextBlock.Text = "Error: " + ex.Message;
+                }
+            }
+        }
+
 
         private void Btn_Back_Click(object sender, RoutedEventArgs e)
         {
